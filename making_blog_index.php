d68d80a5f5bed04b8bc09914b3486e29d4c2092c
akkino <git@github.com>
2019-02-07 02:29:08 +0900

    ブログ一覧ひな形作成

---
+++ making_blog_index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <title>making_blog | 自分のブログを作ってみよう！</title>
+    <link rel ="stylesheet" href="making_blog.css">
+  </head>
+  <body>
+    <header>
+      <div id="header">
+        <div id="logo">
+          <a href="./making_blog_index.php">making blog</a>
+        </div>
+        <nav>
+          ここに他のページリンク
+        </nav>
+      </div>
+    </header>
+    <div id="cover">
+      <h1 id="cover_title">自分のブログを作ってみよう！</h1>
+
+    </div>
+    <div class="wrapper">
+      <div id="main">
+        <div id="blog_list" class="clearfix">
+          ここにブログの記事一覧
+        </div>
+      </div>
+    </div>
+    <footer>
+      <small>© 2019 making blog.</small>
+    </footer>
+  </body>
+</html>
