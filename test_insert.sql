caed58c8f2666da3d715ee4c97bcdb6525cefc05
akkino <git@github.com>
2019-02-09 05:56:58 +0900

    テストデータ作成

---
+++ test_insert.sql
@@ -0,0 +1,6 @@
+INSERT INTO user (id,password,account,email,flag) VALUES ("1","$2y$10$P/tNVtCaaZEjGS8L4673HOYRFfTPwXK7R4LSJYsbb0gj40fQP08RS","テスト太郎","test_tarou@test.com","1");
+INSERT INTO user (id,password,account,email,flag) VALUES ("2","$2y$10$rLaGZ7Q8G6JSvzY/kjV4FeJtjb4fTMaZBW2temFucjjrAeDJ5j692","テストユーザー","test@test.com","1");
+
+
+//testtarou
+//test
